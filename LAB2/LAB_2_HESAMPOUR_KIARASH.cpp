722d707eb9f8d9e8e22d89ba4a8031bdaaa7ff35
LAbeast021 <kiarashhesampour@gmail.com>
2020-10-01 04:13:31 -0700

    lab2 done

--- LAB2/LAB_2_HESAMPOUR_KIARASH.cpp
+++ LAB2/LAB_2_HESAMPOUR_KIARASH.cpp
@@ -10,12 +10,12 @@
 using namespace std;
 
 double powerCalculator(int, int );
-int sumOfSquaresFrom1(int );
+int sumOfSquaresFrom1(int, int );
 int sumOfSquaresFromn(int );
 
 int main(){
     double power_value;
-    int base_number, exponent_number, user_choice, number_of_terms;
+    int base_number, exponent_number, user_choice, number_of_terms, total_square_value;
     char rep = 'Y';
 
     while ( rep != 'N') {
@@ -41,6 +41,7 @@ int main(){
                     cin.clear();
                     cin.ignore(numeric_limits<streamsize>::max(),'\n');
                     cout << "Input error, Please Try again." << endl;
+                    break;
                 }
                 cout << "Please enter the exponent number : " << endl;
                 cin  >> exponent_number;
@@ -48,13 +49,54 @@ int main(){
                     cin.clear();
                     cin.ignore(numeric_limits<streamsize>::max(),'\n');
                     cout << "Input error, Please Try again." << endl;
+                    break;
                 }
                 
                 power_value = powerCalculator ( base_number, exponent_number);
                 cout << base_number << " to the power of " << exponent_number << " = " << power_value << endl; 
                 break;
+
+            case 2:
+                cout << "Please enter the number of terms : " << endl;
+                cin  >> number_of_terms;
+                if(cin.fail()) {
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(),'\n');
+                    cout << "Input error, Please Try again." << endl;
+                    break;
+                } 
+                while ( number_of_terms == 0 || number_of_terms < 0) {
+                    cout << "Entered value cannot be 0 or negative, Please try again." << endl;
+                    cin  >> number_of_terms;
+                } 
+                total_square_value = sumOfSquaresFrom1 (number_of_terms, 1);
+                cout << total_square_value << endl; 
+                break; 
+
+            case 3: 
+            cout << "Please enter the number of terms : " << endl;
+                cin  >> number_of_terms;
+                if(cin.fail()) {
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(),'\n');
+                    cout << "Input error, Please Try again." << endl;
+                    break;
+                } 
+                while ( number_of_terms == 0 || number_of_terms < 0) {
+                    cout << "Entered value cannot be 0 or negative, Please try again." << endl;
+                    cin  >> number_of_terms;
+                } 
+                total_square_value = sumOfSquaresFromn (number_of_terms);
+                cout << total_square_value << endl; 
+                break;
+
+            case 4:
+                cout <<  "Thank you " << endl;
+                rep = 'N';
+                break;     
         
             default:
+                cout << "Entered option is invalid, Please try again : " << endl;
                 break;
         }
 
@@ -75,4 +117,23 @@ double powerCalculator(int base_number, int exponent_number) {
         return 1 / powerCalculator(base_number , -exponent_number);
     }
     
-}   
+} 
+int sumOfSquaresFrom1(int number_of_terms, int initial_value ) {
+    if ( initial_value == number_of_terms ) {
+        cout << "(" << initial_value << " * " << initial_value << ") = " ; 
+        return initial_value * initial_value ;
+    }
+    cout << "(" << initial_value << " * " << initial_value << ") + " ;
+    return initial_value * initial_value + sumOfSquaresFrom1 (number_of_terms , initial_value +1 );
+} 
+int sumOfSquaresFromn(int number_of_terms ) {
+    if(number_of_terms > 1){
+        cout << "(" << number_of_terms << " * " << number_of_terms << ") + ";
+       return number_of_terms * number_of_terms + sumOfSquaresFromn(number_of_terms -1 );
+    }    
+
+   else if(number_of_terms == 1){
+       cout << "(1 * 1) = " ;
+       return 1;
+   }
+}
